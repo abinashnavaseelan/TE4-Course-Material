6c46d07143d222bc0e462817ee26b94a356f4efa
abinashnavaseelan <abinash.navaseelan@outlook.com>
2025-09-15 14:51:22 +0200

    JavaScript Intro

---
+++ JS/JS-01/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let name = "Bob Smith";
+console.log(typeof name, name);
+let age = 49;
+console.log(typeof age, age);
+let is_Student = true;
+console.log(typeof is_Student, is_Student);
+
+age = age + 1;
+console.log(age);
+
+is_Student = false;
+console.log(is_Student);
