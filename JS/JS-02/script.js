8d4fcab323a2a5163839df4fcb7ff63f01beea13
abinashnavaseelan <abinash.navaseelan@outlook.com>
2025-09-16 11:07:40 +0200

    JavaScript Day 2

---
+++ JS/JS-02/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+console.log("Hello, World!");
+
+const first_name = "John";
+const middle_name = "M.";
+const last_name = "Doe";
+const name = `${first_name} ${middle_name} ${last_name}`;
+const hobby = "Roadman";
+const age = 66;
+const county = "UK";
+const quote = "𝚃𝙷𝙸𝚂 𝙸𝚂 𝙼𝚈 𝙿𝙴𝚁𝙵𝙴𝙲𝚃 𝚅𝙸𝙲𝚃𝙾𝚁𝚈, 𝚃𝙷𝙰𝚃𝚂 𝚁𝙸𝙶𝙷𝚃, 𝙸 𝚆𝙸𝙽! - Light";
+
+let day = "16";
+let month = "September";
+let year = "2025";
+let date = `${day} ${month} ${year}`;
+
+console.log(quote);
+console.log(date);
+
+const greeting = `My name is ${name}. I am ${age} years old and I live in ${county} and im a professional ${hobby}.`;
+console.log(greeting);
