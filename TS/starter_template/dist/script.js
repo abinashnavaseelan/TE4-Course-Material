75c3007cd5c6b4de78aec2955f3c6e6bc620fc14
abinashnavaseelan <abinash.navaseelan@outlook.com>
2025-11-10 14:28:15 +0100

    new assignment

---
+++ TS/starter_template/dist/script.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("Hello World");
+let myName = "Abinash";
+let myNumber = 12;
+const message = `${myName} is ${myNumber} years old.`;
+console.log(message);
---
+++ TS/starter_template/script.ts
@@ -0,0 +1,8 @@
+console.log("Hello World");
+
+let myName: string = "Abinash";
+let myNumber: number = 12;
+
+const message = `${myName} is ${myNumber} years old.`;
+
+console.log(message);
